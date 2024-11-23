d8c81a79787e6170d8ab594c6b726ede6ceef35a
goshag45 <goshagugl44@gmail.com>
2024-11-23 18:16:34 +1100

    first person player model and camera are now awesome!

--- player.gd
+++ player.gd
@@ -4,7 +4,7 @@ var speed
 const WALK_SPEED = 5.0
 const SPRINT_SPEED = 8.0
 const JUMP_VELOCITY = 4.5
-const SENSITIVITY = 0.002
+const SENSITIVITY = 0.0013
 
 #head bob stuff
 const BOB_FREQ = 2.0
@@ -18,18 +18,16 @@ var gravity = 9.8
 
 @onready var head = $Head
 @onready var camera = $Head/Camera3D
-@onready var player_model = $Sketchfab_model
+@onready var player = $"."
 
 func _ready():
 	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 	
 func _unhandled_input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		head.rotate_y(-event.relative.x * SENSITIVITY)
+		player.rotate_y(-event.relative.x * SENSITIVITY)
 		camera.rotate_x(-event.relative.y * SENSITIVITY)
 		camera.rotation.x = clamp(camera.rotation.x, deg_to_rad(-60), deg_to_rad(90))
-		
-		player_model.rotation.z = head.rotation.y - deg_to_rad(180)
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
@@ -52,7 +50,7 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
-	var direction = (head.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = -(player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if is_on_floor():
 		if direction:
 			velocity.x = direction.x * speed
