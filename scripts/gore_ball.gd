34ebadcdc95ec549b467ca393b49c280fb899c1d
goshag45 <goshagugl44@gmail.com>
2024-12-15 17:02:34 +1100

    added movement code, wont work until player reference exists

--- scripts/gore_ball.gd
+++ scripts/gore_ball.gd
@@ -1,9 +1,15 @@
 extends CharacterBody3D
 
 @export var health = 200
+@export var player_path  : NodePath #NEED TO REFERENCE PLAYER SOMEHOW
+@export var speed = 4.0
+
+@onready var nav_agent = $NavigationAgent3D
+
+var player = null
 
 func _ready():
-	pass
+	player = get_node(player_path)
 
 func _physics_process(delta: float) -> void:
 	pass
@@ -11,3 +17,8 @@ func _physics_process(delta: float) -> void:
 func _process(delta):
 	if health <= 0:
 		queue_free()
+	velocity = Vector3.ZERO
+	nav_agent.set_target_position(player.global_position.origin)
+	var next_nav_point = nav_agent.get_next_path_position()
+	velocity = (next_nav_point - global_transform.origin).normalized() * speed
+	move_and_slide()
