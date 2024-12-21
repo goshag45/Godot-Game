082ef9cbcb6da9cdb47bf4901177f52a2cf3fc37
goshag45 <goshagugl44@gmail.com>
2024-12-21 15:50:32 +1100

    disabled world triplanar, entity now looks better

--- scripts/gore_ball.gd
+++ scripts/gore_ball.gd
@@ -2,7 +2,7 @@ extends CharacterBody3D
 
 @export var health = 200
 @export var player_path  : NodePath #NEED TO REFERENCE PLAYER SOMEHOW
-@export var speed = 4.0
+@export var speed = 3.0
 
 @onready var nav_agent = $NavigationAgent3D
 @onready var player = get_tree().get_first_node_in_group("player")
@@ -14,7 +14,6 @@ func _physics_process(delta: float) -> void:
 	pass
 
 func _process(delta):
-	print(player)
 	if health <= 0:
 		queue_free()
 	velocity = Vector3.ZERO
