980e183ccbd5e5a13654b8f7fa51592fc6f68725
goshag45 <goshagugl44@gmail.com>
2025-01-11 18:22:25 +1100

    adding scripts, add null check just in case

---
+++ scripts/interact_ray.gd
@@ -0,0 +1,15 @@
+extends RayCast3D
+
+@onready var interact_popup = $"../../../CanvasLayer/Control/interactable_message"
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	var collider = self.get_collider()
+	
+	if self.is_colliding() && collider != null:
+		if collider.is_in_group("interactable_press"):
+			interact_popup.show()
+			if Input.is_action_just_pressed("interact"):
+				collider.interact()
+	else:
+		interact_popup.hide()
