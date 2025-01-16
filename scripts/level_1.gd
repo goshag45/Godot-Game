d53cc0918af60ad21ef30485e85e32638a2a0089
goshag45 <goshagugl44@gmail.com>
2025-01-16 20:03:42 +1100

    gore balls spawning  :)))

--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -1,15 +1,13 @@
 extends Node3D
 
 @onready var spawn_button = $spawn_balls_button
-@onready var spawn_region = $spawn_area
-#var spawn_region_center = spawn_region.global_position
+@onready var spawn_location = $spawn_area/spawn_collision_area
 
 #var deathbox = get_tree().get_first_node_in_group("deathbox")
 var gore_ball = preload("res://scenes/gore_ball.tscn")
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	print(spawn_region)
 	spawn_button.press.connect(spawn_gore_ball)
 	pass
 
@@ -18,6 +16,8 @@ func _process(delta: float) -> void:
 	pass
 
 func spawn_gore_ball():
+	var spawn_region_center = spawn_location.global_position
 	var gore_ball_instance = gore_ball.instantiate()
 	add_child(gore_ball_instance)
-	#gore_ball_instance.global_position = spawn_region_center
+	gore_ball_instance.global_position = spawn_region_center
+	print("spawned goreball at ", spawn_region_center)
