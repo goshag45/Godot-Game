cd80115acf5eaec91c680aa94d18410313232c94
goshag45 <goshagugl44@gmail.com>
2025-01-20 21:38:41 +1100

    fixed crashing - detecting the node successfully but not detecting entering

--- scripts/world.gd
+++ scripts/world.gd
@@ -1,16 +1,23 @@
 extends Node3D
 
-var deathbox = get_tree().get_first_node_in_group("deathbox")
+var deathbox: Area3D = null
 
 func _ready() -> void:
-	get_tree().change_scene_to_file("res://scenes/level_1.tscn")
-	pass
+	#get_tree().change_scene_to_file("res://scenes/level_1.tscn")
+	# Wait for the new scene to load before trying to find the deathbox
+	call_deferred("_setup_deathbox_signal")
+
+func _setup_deathbox_signal() -> void:
+	# Safely locate the deathbox node after the scene is loaded
+	deathbox = get_tree().get_first_node_in_group("deathbox")
+	if deathbox:
+		deathbox.area_entered.connect(_on_deathbox_area_entered)
+		print("connected")
+	else:
+		print("Error: Deathbox node not found")
+
+func _on_deathbox_area_entered(area: Area3D) -> void:
+	print("entered")
 
 func _process(delta: float) -> void:
 	pass
-
-func spawn_gore_ball():
-	pass
-
-func _on_deathbox_area_entered(area: Area3D) -> void:
-	pass # Replace with function body.
