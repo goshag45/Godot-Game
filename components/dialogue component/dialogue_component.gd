9b762e1c4c8dadd8f4ba0e0f725f418df802bada
goshag45 <goshagugl44@gmail.com>
2025-01-31 19:01:21 +1100

    working on dialogue component

--- components/dialogue component/dialogue_component.gd
+++ components/dialogue component/dialogue_component.gd
@@ -1,11 +1,16 @@
 extends Control
 
+@onready var dialogue_component = $"."
+@onready var text = $text
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func _showdialogue(_text: String):
+	text.text = _text
+	dialogue_component.visible()
