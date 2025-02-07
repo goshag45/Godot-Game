1e68fbf162afbdc9830b8031a273a2bb89313d5f
goshag45 <goshagugl44@gmail.com>
2025-02-07 18:43:38 +1100

    thedavy will now be looking at the player

--- scripts/thedavy.gd
+++ scripts/thedavy.gd
@@ -6,7 +6,8 @@ extends CharacterBody3D
 @onready var dialogue_component = $dialogue_component
 
 signal talk()
-# Called when the node enters the scene tree for the first time.
+
+#Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var players = get_tree().get_nodes_in_group("player")
 	if players == null:
@@ -18,11 +19,11 @@ func _ready() -> void:
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	if player:
-		thedavy.look_at(player.global_position)
-	pass
+		var direction = (player.global_transform.origin - global_transform.origin).normalized()
+		direction.y = 0  # Prevent tilting by locking the Y-axis
+		look_at(global_transform.origin - direction, Vector3.UP)
 
 func interact():
 	audio_component._play_audio_sfx("icanseeyou", 10.0)
 	dialogue_component._showdialogue("i can see you")
 	talk.emit()
-	pass
