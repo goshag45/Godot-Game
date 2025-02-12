32fa69a8e81a6a16c951b8fb63f62d5402f1d286
goshag45 <goshagugl44@gmail.com>
2025-02-12 21:28:32 +1100

    trying to get pause menu working

--- components/input/input_controller.gd
+++ components/input/input_controller.gd
@@ -19,3 +19,7 @@ func _pause_game():
 		else:
 			Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
 			pause_menu.hide()
+
+#func _input(event):
+	#if event is InputEventMouseButton:
+		#print("Mouse clicked: ", event.position)
--- scripts/pause_menu.gd
+++ scripts/pause_menu.gd
@@ -1,17 +1,7 @@
 extends Control
 
-
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
-
-
 func _on_play_pressed() -> void:
+	get_tree().paused = false
 	pass # Replace with function body.
 
 func _on_exit_pressed() -> void:
