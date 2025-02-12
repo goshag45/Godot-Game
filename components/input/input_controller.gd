d2bbde9604991fa089e0d37076e9b93b43b0b3ef
goshag45 <goshagugl44@gmail.com>
2025-02-12 20:59:39 +1100

    working on pause button implementation

--- components/input/input_controller.gd
+++ components/input/input_controller.gd
@@ -4,10 +4,6 @@ extends Node
 
 var is_paused : bool = false
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	_pause_game()
