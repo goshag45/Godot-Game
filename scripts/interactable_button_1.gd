f9a7b119e60420e592da81176645a1d14a67affd
goshag45 <goshagugl44@gmail.com>
2025-03-02 21:32:51 +1100

    tried fixing the blood emission reload bug VERY difficult

--- scripts/interactable_button_1.gd
+++ scripts/interactable_button_1.gd
@@ -4,7 +4,6 @@ extends StaticBody3D
 @onready var dialogue_component = $dialogue_component
 
 signal press()
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta: float) -> void:
 	pass
 
