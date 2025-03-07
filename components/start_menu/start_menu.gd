74268a9764754e0076714b80238f1ed76b105abe
goshag45 <goshagugl44@gmail.com>
2025-03-07 20:45:10 +1100

    working on home menu

---
+++ components/start_menu/start_menu.gd
@@ -0,0 +1,15 @@
+extends Control
+
+@onready var start_menu = $"."
+
+func ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _on_play_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_options_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_exit_pressed() -> void:
+	pass # Replace with function body.
