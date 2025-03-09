d37bc8c9ec1ed3853899400250a894a9021b783e
goshag45 <goshagugl44@gmail.com>
2025-03-09 22:44:58 +1100

    fixed it!

--- components/player_spawn_component/player_spawn_component.gd
+++ components/player_spawn_component/player_spawn_component.gd
@@ -1,5 +1,4 @@
 extends Node3D
 
 func spawn_at(location: Node3D):
-	print(get_parent())
-	#get_parent().global_transform = location.global_transform
+	get_parent().global_position = location.global_position
--- scripts/level_1.gd
+++ scripts/level_1.gd
@@ -2,6 +2,7 @@ extends Node3D
 
 @onready var spawn_button = $world/spawn_balls_button
 @onready var ball_spawn_location = $world/spawn_area/spawn_collision_area
+@onready var spawn_location_1 = $world/spawn_location_1
 
 #var deathbox = get_tree().get_first_node_in_group("deathbox")
 var gore_ball = preload("res://scenes/npc/gore_ball.tscn")
@@ -11,7 +12,7 @@ func _ready() -> void:
 	spawn_button.press.connect(spawn_gore_ball)
 	var player_instance = player_scene.instantiate()
 	add_child(player_instance)
-	#player_instance.get_node("player_spawn_component").spawn_at($spawn_location_1)
+	player_instance.get_node("player_spawn_component").spawn_at(spawn_location_1)
 
 func _process(_delta: float) -> void:
 	pass
@@ -21,4 +22,3 @@ func spawn_gore_ball():
 	var gore_ball_instance = gore_ball.instantiate()
 	add_child(gore_ball_instance)
 	gore_ball_instance.global_position = ball_spawn_region_center
-	#print("spawned goreball at ", ball_spawn_region_center)
