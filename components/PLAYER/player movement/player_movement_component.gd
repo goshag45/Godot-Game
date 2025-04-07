692d88587008ebce144e232864981c3291ff11a6
goshag45 <goshagugl44@gmail.com>
2025-04-07 19:03:18 +1000

    movement code put into component

--- components/PLAYER/player movement/player_movement_component.gd
+++ components/PLAYER/player movement/player_movement_component.gd
@@ -9,14 +9,24 @@ extends Node3D
 var direction = Vector3()
 var jump_sounds = ["jump1", "jump2", "jump3"]
 
-var walk_speed = player.walk_speed
-var sprint_speed = player.sprint_speed
-var jump_velocity = player.jump_velocity
-var base_fov = player.base_fov
-var fov_scale = player.fov_scale
-var gravity = player.gravity
-var speed = player.speed
-var health = player.health
+var walk_speed : float
+var sprint_speed : float
+var jump_velocity : float
+var base_fov : int
+var fov_scale : float
+var gravity : float
+var speed : float
+var health : int
+
+func _ready() -> void:
+	walk_speed = player.walk_speed
+	sprint_speed = player.sprint_speed
+	jump_velocity = player.jump_velocity
+	base_fov = player.base_fov
+	fov_scale = player.fov_scale
+	gravity = player.gravity
+	speed = player.walk_speed
+	health = player.health
 
 func _physics_process(delta: float) -> void:
 		# Add the gravity.
@@ -46,19 +56,19 @@ func _physics_process(delta: float) -> void:
 	# Get the input direction and handle the movement/deceleration.
 	var input_dir = Input.get_vector("left", "right", "forward", "back")
 	direction = (player.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	print(direction)
 	if player.is_on_floor():
 		if direction:
-			player.velocity.x = player.direction.x * speed
-			player.velocity.z = player.direction.z * speed
+			player.velocity.x = direction.x * speed
+			player.velocity.z = direction.z * speed
 		else:
 			player.velocity.x = 0.0
 			player.velocity.z = 0.0
 	else:
-		player.velocity.x = lerp(player.velocity.x, player.direction.x * player.speed, delta * 4.0)
-		player.velocity.z = lerp(player.velocity.z, player.direction.z * player.speed, delta * 4.0)
+		player.velocity.x = lerp(player.velocity.x, direction.x * speed, delta * 4.0)
+		player.velocity.z = lerp(player.velocity.z, direction.z * speed, delta * 4.0)
 
 	player.move_and_slide()
-
 
 func jump():
 	player.velocity.y = jump_velocity
