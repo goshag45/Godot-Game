660e61e6017e6bcc8a451ce2552e28162e97298e
goshag45 <goshagugl44@gmail.com>
2025-06-27 17:06:40 +1000

    accurate player spawning in trenchbroom level

--- trenchbroom_levels/trenchbroom_map_1.gd
+++ trenchbroom_levels/trenchbroom_map_1.gd
@@ -1,7 +1,19 @@
 extends Node3D
 
+@export var player_spawn_location_1 : Node3D
 var player_scene = preload("res://scenes/player/player.tscn")
 
 func _ready() -> void:
+	add_player()
+
+func _on_deathbox_body_entered(body: Node3D) -> void:
+	if body.is_in_group("player"):
+		get_tree().reload_current_scene()
+	if body.is_in_group("enemy"):
+		body.die()
+
+func add_player():
 	var player_instance = player_scene.instantiate()
 	add_child(player_instance)
+	player_instance.get_node("player_spawn_component").spawn_at(player_spawn_location_1)
+	player_instance.audio_component._play_audio_sfx("vine_boom", 8.0, false)
